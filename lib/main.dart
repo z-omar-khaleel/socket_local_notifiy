ecc9a193f3ca16a58d2db243161a822d3e125220
z-omar-khaleel <ozul-1985@hotmail.com>
2021-05-26 03:22:22 +0300

    This is socket and local notification and getx state mangment

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:intl/intl.dart';
+import 'package:task1_implemention/socket.dart';
+import 'package:web_socket_channel/io.dart';
+
+import 'get.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,10 +13,13 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Home(),
-    );
+    return GetMaterialApp(
+        initialBinding: IntinalBinding(),
+        debugShowCheckedModeBanner: false,
+        home: new MyHomePage(
+          channel: new IOWebSocketChannel.connect(
+              Uri.parse('wss://jawali-api.herokuapp.com')),
+        ));
   }
 }
 
@@ -22,149 +30,190 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Task 1'),
-        ),
-        body: Padding(
-          padding: EdgeInsets.all(15),
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Add Course ',
-                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-                ),
-                Form(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Flexible(
-                        flex: 1,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            TextFormField(
-                              decoration: InputDecoration(
-                                  labelText: 'Course Name',
-                                  hintText: 'Enter Course Name'),
-                              validator: (val) {
-                                if (val.isEmpty) return 'Enter Course Name';
-                                return null;
-                              },
-                              keyboardType: TextInputType.text,
-                            ),
-                            TextFormField(
-                                decoration: InputDecoration(
-                                    labelText: 'course Instructor',
-                                    hintText: 'Enter Course Instructor'),
-                                validator: (val) {
-                                  if (val.isEmpty) {
-                                    return 'Enter Course Instructor';
-                                  }
-                                  return null;
-                                },
-                                keyboardType: TextInputType.text),
-                            TextFormField(
-                                controller: startController,
-                                decoration: InputDecoration(
-                                    labelText: 'Start Date',
-                                    hintText: 'Enter Start Date'),
-                                validator: (val) {
-                                  if (val.isEmpty) return 'Enter Start Date';
-                                  return null;
-                                },
-                                onTap: () {
-                                  showDatePicker(
-                                    context: context,
-                                    initialDate: DateTime.now(),
-                                    firstDate: DateTime.now(),
-                                    lastDate: DateTime.parse('2021-12-03'),
-                                  ).then((value) {
-                                    startController.text =
-                                        DateFormat.yMMMd().format(value);
-                                  });
-                                },
-                                keyboardType: TextInputType.datetime),
-                            TextFormField(
-                                controller: endController,
-                                onTap: () {
-                                  showDatePicker(
-                                    context: context,
-                                    initialDate: DateTime.now(),
-                                    firstDate: DateTime.now(),
-                                    lastDate: DateTime.parse('2021-12-03'),
-                                  ).then((value) {
-                                    endController.text =
-                                        DateFormat.yMMMd().format(value);
-                                  });
-                                },
-                                decoration: InputDecoration(
-                                    labelText: ' End Date',
-                                    hintText: 'Enter End Date'),
-                                validator: (val) {
-                                  if (val.isEmpty) {
-                                    return 'Enter End Date';
-                                  }
-                                  if (val.isNotEmpty) {
-                                    print('/*/*/**/*/');
-                                    final DateTime start = DateFormat.yMMMd()
-                                        .parse(startController.text);
-                                    print(start);
-                                    final DateTime end = DateFormat.yMMMd()
-                                        .parse(endController.text);
-                                    print(end);
+      appBar: AppBar(
+        title: Text('Task 1'),
+      ),
+      body: OtherClass(),
+    );
+  }
+}
 
-                                    if (start.isAfter(end))
-                                      return 'Enter End Date after Start';
-                                  }
-                                  return null;
-                                },
-                                keyboardType: TextInputType.datetime),
-                            TextFormField(
-                                decoration: InputDecoration(
-                                    labelText: 'Course Duration',
-                                    hintText: 'Enter Course Duration'),
-                                validator: (val) {
-                                  if (val.isEmpty)
-                                    return 'Enter Course Duration';
-                                  return null;
-                                },
-                                keyboardType: TextInputType.number),
-                            TextFormField(
-                              decoration: InputDecoration(
-                                  labelText: 'Maximum number of Students',
-                                  hintText: 'Enter Maximum number of Students'),
-                              validator: (val) {
-                                if (val.isEmpty)
-                                  return 'Enter Maximum number of Students';
-                                return null;
-                              },
-                              keyboardType: TextInputType.number,
-                            ),
-                          ],
-                        ),
-                      ),
-                      SizedBox(
-                        height: 40,
-                      ),
-                      // ignore: deprecated_member_use
-                      RaisedButton(
-                        onPressed: () {
-                          if (_key.currentState.validate()) {}
-                        },
-                        child: Text('Submit'),
-                      )
-                    ],
-                  ),
-                  key: _key,
-                )
-              ],
+class Universty extends StatelessWidget {
+  Universty({
+    @required this.startController,
+    @required this.endController,
+    @required this.key,
+  });
+
+  final TextEditingController startController;
+  final TextEditingController endController;
+  final GlobalKey<FormState> key;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(15),
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Add Course ',
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
             ),
-          ),
-        ));
+            Form(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Flexible(
+                    flex: 1,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        TextFormField(
+                          decoration: InputDecoration(
+                              labelText: 'Course Name',
+                              hintText: 'Enter Course Name'),
+                          validator: (val) {
+                            if (val.isEmpty) return 'Enter Course Name';
+                            return null;
+                          },
+                          keyboardType: TextInputType.text,
+                        ),
+                        TextFormField(
+                            decoration: InputDecoration(
+                                labelText: 'course Instructor',
+                                hintText: 'Enter Course Instructor'),
+                            validator: (val) {
+                              if (val.isEmpty) {
+                                return 'Enter Course Instructor';
+                              }
+                              return null;
+                            },
+                            keyboardType: TextInputType.text),
+                        TextFormField(
+                            controller: startController,
+                            decoration: InputDecoration(
+                                labelText: 'Start Date',
+                                hintText: 'Enter Start Date'),
+                            validator: (val) {
+                              if (val.isEmpty) return 'Enter Start Date';
+                              return null;
+                            },
+                            onTap: () {
+                              showDatePicker(
+                                context: context,
+                                initialDate: DateTime.now(),
+                                firstDate: DateTime.now(),
+                                lastDate: DateTime.parse('2021-12-03'),
+                              ).then((value) {
+                                startController.text =
+                                    DateFormat.yMMMd().format(value);
+                              });
+                            },
+                            keyboardType: TextInputType.datetime),
+                        TextFormField(
+                            controller: endController,
+                            onTap: () {
+                              showDatePicker(
+                                context: context,
+                                initialDate: DateTime.now(),
+                                firstDate: DateTime.now(),
+                                lastDate: DateTime.parse('2021-12-03'),
+                              ).then((value) {
+                                endController.text =
+                                    DateFormat.yMMMd().format(value);
+                              });
+                            },
+                            decoration: InputDecoration(
+                                labelText: ' End Date',
+                                hintText: 'Enter End Date'),
+                            validator: (val) {
+                              if (val.isEmpty) {
+                                return 'Enter End Date';
+                              }
+                              if (val.isNotEmpty) {
+                                print('/*/*/**/*/');
+                                final DateTime start = DateFormat.yMMMd()
+                                    .parse(startController.text);
+                                print(start);
+                                final DateTime end = DateFormat.yMMMd()
+                                    .parse(endController.text);
+                                print(end);
+
+                                if (start.isAfter(end))
+                                  return 'Enter End Date after Start';
+                              }
+                              return null;
+                            },
+                            keyboardType: TextInputType.datetime),
+                        TextFormField(
+                            decoration: InputDecoration(
+                                labelText: 'Course Duration',
+                                hintText: 'Enter Course Duration'),
+                            validator: (val) {
+                              if (val.isEmpty) return 'Enter Course Duration';
+                              return null;
+                            },
+                            keyboardType: TextInputType.number),
+                        TextFormField(
+                          decoration: InputDecoration(
+                              labelText: 'Maximum number of Students',
+                              hintText: 'Enter Maximum number of Students'),
+                          validator: (val) {
+                            if (val.isEmpty)
+                              return 'Enter Maximum number of Students';
+                            return null;
+                          },
+                          keyboardType: TextInputType.number,
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 40,
+                  ),
+                  // ignore: deprecated_member_use
+                  RaisedButton(
+                    onPressed: () {
+                      if (key.currentState.validate()) {}
+                    },
+                    child: Text('Submit'),
+                  )
+                ],
+              ),
+              key: key,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class OtherClass extends GetView<Controller> {
+  @override
+  // TODO: implement controller
+
+  @override
+  Widget build(BuildContext context) {
+    controller.change(User('zeco', 'title'), status: RxStatus.error('Error'));
+    return Scaffold(
+      body: controller.obx(
+        (state) => Center(child: Text(state.title)),
+
+        // here you can put your custom loading indicator, but
+        // by default would be Center(child:CircularProgressIndicator())
+        onLoading: CircularProgressIndicator(),
+        onEmpty: Text('No data found'),
+
+        // here also you can set your own error widget, but by
+        // default will be an Center(child:Text(error))
+        onError: (error) => Text(error),
+      ),
+    );
   }
 }
---
+++ lib/socket.dart
@@ -0,0 +1,134 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:meta/meta.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
+import 'package:web_socket_channel/io.dart';
+
+class MyHomePage extends StatefulWidget {
+  final WebSocketChannel channel;
+  MyHomePage({this.channel});
+
+  @override
+  MyHomePageState createState() {
+    return new MyHomePageState();
+  }
+}
+
+class MyHomePageState extends State<MyHomePage> {
+  TextEditingController editingController = new TextEditingController();
+  int len = 0;
+  List data = [];
+  Stream stream;
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
+  Future _showNotification(String title) async {
+    var androidPlatformChannelSpecifics = new AndroidNotificationDetails(
+        'your channel id', 'your channel name', 'your channel description',
+        importance: Importance.max, priority: Priority.high);
+    var iOSPlatformChannelSpecifics = new IOSNotificationDetails();
+    var platformChannelSpecifics = new NotificationDetails(
+        android: androidPlatformChannelSpecifics,
+        iOS: iOSPlatformChannelSpecifics);
+    await flutterLocalNotificationsPlugin.show(
+      0,
+      'New Notification',
+      '$title',
+      platformChannelSpecifics,
+      payload: 'This is notification detail Text...',
+    );
+  }
+
+  Future onSelectNotification(String payload) async {
+    //لما يضغط عليها
+    showDialog(
+      context: context,
+      builder: (_) {
+        return new AlertDialog(
+          title: Text("Your Notification Detail"),
+          content: Text("Payload : $payload"),
+        );
+      },
+    );
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    var initializationSettingsAndroid =
+        new AndroidInitializationSettings('app_icon');
+    var initializationSettingsIOS = new IOSInitializationSettings();
+    var initializationSettings = new InitializationSettings(
+        android: initializationSettingsAndroid, iOS: initializationSettingsIOS);
+    flutterLocalNotificationsPlugin = new FlutterLocalNotificationsPlugin();
+    flutterLocalNotificationsPlugin.initialize(initializationSettings,
+        onSelectNotification: onSelectNotification);
+
+    stream = widget.channel.stream.asBroadcastStream();
+    stream.listen((event) {
+      print(event);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Web Socket"),
+      ),
+      body: new Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: new Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            new Form(
+              child: new TextFormField(
+                decoration: new InputDecoration(labelText: "Send any message"),
+                controller: editingController,
+              ),
+            ),
+            Expanded(
+              child: new StreamBuilder(
+                stream: stream,
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                  if (snapshot.hasData) {
+                    data.add(snapshot.data);
+                    data = data.reversed.toList();
+                  }
+                  return ListView.builder(
+                    itemBuilder: (c, i) => Text(data[i]),
+                    itemCount: data.length,
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: new FloatingActionButton(
+        child: new Icon(Icons.send),
+        onPressed: _sendMyMessage,
+      ),
+    );
+  }
+
+  void _sendMyMessage() {
+    if (editingController.text.isNotEmpty) {
+      widget.channel.sink.add(editingController.text);
+      stream.listen((event) {
+        _showNotification(event.toString());
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    widget.channel.sink.close();
+
+    super.dispose();
+  }
+}
